6607971c0b3a13e04499bfaca94fc928cbd20f05
bonnie chang <b22379222@yahoo.com.tw>
2017-12-19 09:34:55 +0800

    roll the dice

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -10,22 +10,19 @@ import java.util.Random;
 
 public class Func1Activity extends AppCompatActivity {
     public Button broll;
-    int ram= new Random().nextInt(6) + 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func1);
         broll = (Button) findViewById(R.id.b_roll);
-        broll.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (view.getId() == R.id.b_roll) {
-                    new AlertDialog.Builder(Func1Activity.this)
-                            .setMessage("亂數為" + ram)
-                            .setPositiveButton("OK", null)
-                            .show();
-                }
-            }
-    });
+    }
+    public void mid2(View view) {
+        int n = new Random().nextInt(6) + 1;
+        String num = Integer.toString(n);
+        new AlertDialog.Builder(this)
+                .setMessage("亂數為" + num)
+                .setPositiveButton("OK", null)
+                .show();
     }
 }
